bc2b0fb1bfdf5c1643eaf2c6dfcb830e0c592335
Romariin <me@romarin.dev>
2026-01-29 11:59:53 +0100

    refactor: remove ServerTab and ResourcesTab components from configurator index

--- docs/components/home/configurator/index.ts
+++ docs/components/home/configurator/index.ts
@@ -2,8 +2,6 @@ export * from './types';
 export { Configurator } from './Configurator';
 export { ModCard } from './ModCard';
 export { GeneralTab } from './GeneralTab';
-export { ServerTab } from './ServerTab';
-export { ResourcesTab } from './ResourcesTab';
 export { ModsTab } from './ModsTab';
 export { MemoryBar } from './MemoryBar';
 export { ComposePreview } from './ComposePreview';
